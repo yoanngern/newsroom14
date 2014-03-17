3bae2a77a058a918289266b25957d0eaf2f4c105
Yoann Gern <webyg@icloud.com>
2014-03-17 09:36:22 +0100

    Delete README

---
+++ app/cache/dev/twig/1c/c4/43f68e2434266fcdafa7a5b56cd0fb0aa91ccd80a375c542c2b5c5698cb8.php
@@ -0,0 +1,51 @@
+<?php
+
+/* comemNewsroomBundle:Default:home.html.twig */
+class __TwigTemplate_1cc443f68e2434266fcdafa7a5b56cd0fb0aa91ccd80a375c542c2b5c5698cb8 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("::layout.html.twig");
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "::layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        // line 4
+        echo "
+This is the home page.
+
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "comemNewsroomBundle:Default:home.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 4,  28 => 3,);
+    }
+}
