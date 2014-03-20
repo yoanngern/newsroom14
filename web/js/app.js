47e6d6fae0233076287a2127ab4731963e8ff5c5
FlavienKnuchel <knuchelf@gmail.com>
2014-03-20 17:15:44 +0100

    début de la recherche

---
+++ web/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#search').keyup(function(){
+        var search = "http://localhost/newsroom14/web/app_dev.php/annuaire.json?search="+$("#search").val();
+        $.get(search, function(data){
+            $("#results").append(data);
+            console.log(data);
+        })
+    });
+});
